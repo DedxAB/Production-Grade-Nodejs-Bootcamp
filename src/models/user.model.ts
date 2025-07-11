a1d408f8afc0e91ec16ec8a58af0d5274034e492
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-07-12 04:01:26 +0530

    fix: hide password field in user model queries by default

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false }, // Password should not be returned in queries by default
   },
   { timestamps: true }
 );
