2c7ddc4118ddb61ce74c2cce588e1c7ba41b447a
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-08-19 20:12:35 +0530

    feat: enhance security and performance with rate limiting, XSS protection, and HTTP parameter pollution prevention

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import cors from 'cors';
-import express, { Request, Response } from 'express';
+import express from 'express';
+import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import hpp from 'hpp';
 import swaggerUi from 'swagger-ui-express';
+import xssClean from 'xss-clean';
 
 import { config } from './config/index.js';
 import { globalErrorHandler } from './middlewares/globalError.middleware.js';
 import apiRouter from './routes/api.routes.js';
+import { AppError } from './utils/appError.js';
 import { logger } from './utils/logger.js';
 import { swaggerSpec } from './utils/swagger.js';
 
@@ -13,7 +17,9 @@ export const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(xssClean()); // Prevent XSS attacks
+app.use(hpp()); // Prevent HTTP param pollution
+app.use(express.json({ limit: '100kb' }));
 app.disable('x-powered-by');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
@@ -22,10 +28,19 @@ app.use((req, _res, next) => {
   next();
 });
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Too many requests from this IP, please try again later.',
+});
+app.use(config.ROOT_API, limiter);
+
 app.use(config.ROOT_API, apiRouter);
 
-app.use('*', (_req: Request, res: Response) => {
-  res.status(404).json({ message: 'Route not found' });
+app.all('*', (req, _res, next) => {
+  next(new AppError(`Route ${req.originalUrl} not found`, 404));
 });
 
 app.use(globalErrorHandler);
